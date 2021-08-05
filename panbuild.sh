943256bd7402b54c2ebe64dd3126d7f1ea925ea3
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2021-08-05 19:32:18 -0400

    Update panbuild.sh

--- panbuild.sh
+++ panbuild.sh
@@ -5,11 +5,11 @@ PANBUILD_PATH=$HOME/.dotfiles/bin/pandoc/data
 FILE_NAME=$(echo "$1" | cut -f1 -d'.')
 STYLE=zenburn
 PDF_ENGINE=xelatex
-LATEX_TEMPLATE="$PANBUILD_PATH/default-md.tex"
-LATEX_DEPENDENCIES="$PANBUILD_PATH/pandoc-md.tex"
+LATEX_TEMPLATE="$PANBUILD_PATH/school-default-md.tex"
+LATEX_DEPENDENCIES="$PANBUILD_PATH/pandoc-md-dependencies.tex"
 
 pandoc -H "$LATEX_DEPENDENCIES" \
-	  "$PANBUILD_PATH/pandoc-md.yaml" -o "$FILE_NAME"."$2"  "$1".md	\
+	  "$PANBUILD_PATH/school-pandoc-md.yaml" -o "$FILE_NAME"."$2"  "$1".md	\
 	--pdf-engine="$PDF_ENGINE" 		\
 	--highlight-style="$STYLE" 		\
 	--template="$LATEX_TEMPLATE"		\
