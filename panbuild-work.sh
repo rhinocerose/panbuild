effa76ef4327b05b4b9cae61add35e2fd33ec557
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2021-08-06 10:23:54 -0400

    Create panbuild-work.sh

---
+++ panbuild-work.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PANBUILD_PATH=./data
+
+FILE_NAME=$(echo "$1" | cut -f1 -d'.')
+STYLE=zenburn
+PDF_ENGINE=xelatex
+LATEX_TEMPLATE="$PANBUILD_PATH/prof-default-md.tex"
+LATEX_DEPENDENCIES="$PANBUILD_PATH/pandoc-md-dependencies.tex"
+
+pandoc -H "$LATEX_DEPENDENCIES" \
+	  "$PANBUILD_PATH/prof-pandoc-md.yaml" -o "$FILE_NAME"."$2"  "$1".md	\
+	--pdf-engine="$PDF_ENGINE" 		\
+	--highlight-style="$STYLE" 		\
+	--template="$LATEX_TEMPLATE"		\
+	--listings				\
+  --variable subparagraph			\
