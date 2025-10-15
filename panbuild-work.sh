ea063b165453cfc9e3a5a87c63796112df9a363a
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2025-10-15 11:48:39 -0400

    Update panbuild-work.sh

--- panbuild-work.sh
+++ panbuild-work.sh
@@ -3,14 +3,15 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P || exit )"
 PANBUILD_PATH="$SCRIPTPATH"/data
 
-FILE_NAME=$(echo "$1" | cut -f1 -d'.')
+INPUT="$1"
+FILE_NAME="${INPUT%.*}"
 STYLE=zenburn
 PDF_ENGINE=xelatex
 LATEX_TEMPLATE="$PANBUILD_PATH/prof-default-md.tex"
 LATEX_DEPENDENCIES="$PANBUILD_PATH/pandoc-md-dependencies.tex"
 
 pandoc -H "$LATEX_DEPENDENCIES" "$PANBUILD_PATH/prof-metadata.yaml" \
-    -o "$FILE_NAME"."$2"  "$1".md	\
+    -o "$FILE_NAME"."$2"  "$1"	\
 	--pdf-engine="$PDF_ENGINE" 		\
 	--highlight-style="$STYLE" 		\
 	--template="$LATEX_TEMPLATE"		\
